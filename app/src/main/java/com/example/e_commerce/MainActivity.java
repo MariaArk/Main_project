42b0ecb825e43969d0e4000bb69db98ce84a7552
MariaArk <91568827+MariaArk@users.noreply.github.com>
2021-12-23 14:31:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.e_commerce.adapter.CategoryAdapter;
+import com.example.e_commerce.adapter.CourseAdapter;
+import com.example.e_commerce.model.Category;
+import com.example.e_commerce.model.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    RecyclerView categoryRecycler, courseRecycler;
+    CategoryAdapter categoryAdapter;
+    static CourseAdapter courseAdapter;
+    static List<Course> courseList = new ArrayList<>();
+    static List<Course> fullCourseList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1,"Игры"));
+        categoryList.add(new Category(2,"Сайты"));
+        categoryList.add(new Category(3,"Языки"));
+        categoryList.add(new Category(4,"Прочее"));
+
+        setCategoryRecycler(categoryList);
+
+
+        courseList.add(new Course(1,"java", "Профессия Java\nразработчик", "1 января","начальный","#424345", "csmlcm",3));
+        courseList.add(new Course(2,"python_3", "Профессия Python\nразработчик","10 января","начальный","#9FA52D", "jidjijd",3));
+
+        fullCourseList.addAll(courseList);
+
+        setCourseRecycler(courseList);
+    }
+
+    public void openShoppingCart(View view){
+        Intent intent = new Intent(this, OrderPage.class);
+        startActivity(intent);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this,courseList);
+        courseRecycler.setAdapter(courseAdapter);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this,categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+    }
+    public static void showCoursesByCategory(int category){
+
+        courseList.clear();
+        courseList.addAll(fullCourseList);
+
+        List<Course> filterCourses = new ArrayList<>();
+
+        for (Course c:courseList){
+            if(c.getCategory() == category) filterCourses.add(c);
+        }
+
+        courseList.clear();
+        courseList.addAll(filterCourses);
+
+        courseAdapter.notifyDataSetChanged();
+
+    }
+}
---
+++ app/src/main/java/com/example/e_commerce/OrderPage.java
@@ -0,0 +1,31 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.e_commerce.model.Course;
+import com.example.e_commerce.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_page);
+
+        ListView order_list = findViewById(R.id.order_list);
+
+        List<String> coursesTitle = new ArrayList<>();
+        for(Course c: MainActivity.fullCourseList){
+            if(Order.itemsId.contains(c.getId())) coursesTitle.add(c.getTitle());
+        }
+
+        order_list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, coursesTitle));
+    }
+}
